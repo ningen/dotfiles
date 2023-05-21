6104a960a5850dd7ed3cf72395f8627d76da9826
ningen <msg1801022@gmail.com>
2023-05-21 22:29:22 +0900

    simple lsp configuration

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,10 +1,12 @@
-local opt    = vim.opt
-local keymap = vim.keymap
+function set_options(obj, configs)
+  for key, value in pairs(configs) do
+    obj[key] = value
+  end
+end
 
-vim.g.mapleader = ','
+vim.g.mapleader = '<Space>'
 
-
-local configs = {
+local vim_opt = {
   number = true,
   expandtab = true,
   tabstop = 2,
@@ -12,75 +14,132 @@ local configs = {
   signcolumn = 'yes'
 }
 
-for key, value in pairs(configs) do
-  opt[key] = value
-end
 
-opt.clipboard:prepend { "unnamed", "unnamedplus" }
-
-keymap.set('n', '<Tab>', ':tabNext<CR>')
-keymap.set('n', '<S-Tab>', ':tabprevious<CR>')
-
-keymap.set('i', 'jj', '<Esc>')
-
-local status, packer = pcall(require, "packer")
-if (not status) then
-  print("Packer is not installed")
-  return
-end
-
+set_options(vim.opt, vim_opt)
 
 vim.cmd [[packadd packer.nvim]]
 
-packer.startup(function(use)
+require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
-  use 'neovim/nvim-lspconfig' -- lsp
   use {
-    'williamboman/mason.nvim', -- lsp server management
-    requires = {
-      { 'williamboman/mason-lspconfig.nvim' }
-    }
-  }
-  use {
-    'glepnir/lspsaga.nvim',
-    branch = 'main',
-    requires = {
-      { 'neovim/nvim-lspconfig' }
-    }
-  }
+    'EdenEast/nightfox.nvim',
+    config = function()
+      require('nightfox').setup({})
+    end
+  } -- colorscheme
 
-  -- 補完関係
-  use {
-    'hrsh7th/nvim-cmp', -- complementj
-    requires = {
-      { 'hrsh7th/cmp-nvim-lsp' },
-      { 'hrsh7th/vim-vsnip', },
-    }
-  }
-  use 'onsails/lspkind.nvim'
-  use 'L3MON4D3/LuaSnip'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-buffer'
-
-  use {
-    'nvim-telescope/telescope.nvim', -- fuzzy finder
-    requires = {
-      { 'nvim-lua/plenary.nvim' }
-    }
-  }
-
-  use 'nvim-telescope/telescope-file-browser.nvim' -- filer
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = {
-      { 'nvim-tree/nvim-web-devicons' }
-    },
-  }
-  use 'dinhhuy258/git.nvim' --git
-
-  use 'EdenEast/nightfox.nvim' -- colorscheme
   use 'nvim-tree/nvim-web-devicons' -- icon
 
-  use 'voldikss/vim-translator' -- translator(auto -> jp)
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate',
+    config = function()
+      require('nvim-treesitter.configs').setup({})
+    end
+  }
+
+  use 'neovim/nvim-lspconfig'
+  use 'williamboman/mason.nvim'
+  use 'williamboman/mason-lspconfig.nvim'
+
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/vim-vsnip'
+
+
 end)
+
+
+-- 1. LSP Server management
+require('mason').setup()
+require('mason-lspconfig').setup_handlers({
+  function(server)
+    local lspconfigs = {
+      lua_ls = {
+        on_attach = function(client, bufnr)
+          local opts = { nnoremap = true, slient = true }
+          vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+          vim.cmd [[
+            autocmd BufWritePre * lua vim.lsp.buf.formatting_sync(nil, 1000)
+          ]]
+        end,
+        settings = {
+          Lua = {
+            runtime = {
+              version = 'LuaJIT'
+            },
+            diagnostics = { globals = 'vim' },
+            workspace = { library = vim.api.nvim_get_runtime_file("", true) }
+          }
+        }
+      },
+      hls = {},
+      denols = {},
+      tsserver = {},
+      marksman = {}
+    }
+
+    local using_config = lspconfigs[server]
+    require('lspconfig')[server].setup(using_config)
+  end
+})
+
+vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')
+vim.keymap.set('n', 'gf', '<cmd>lua vim.lsp.buf.format({ async = true })<CR>')
+vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
+vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
+vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
+vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
+vim.keymap.set('n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
+vim.keymap.set('n', 'gn', '<cmd>lua vim.lsp.buf.rename()<CR>')
+vim.keymap.set('n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+vim.keymap.set('n', 'ge', '<cmd>lua vim.diagnostic.open_float()<CR>')
+vim.keymap.set('n', 'g]', '<cmd>lua vim.diagnostic.goto_next()<CR>')
+vim.keymap.set('n', 'g[', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
+
+
+-- LSP handlers
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false }
+)
+-- Reference highlight
+vim.cmd [[
+set updatetime=500
+highlight LspReferenceText  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+highlight LspReferenceRead  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+highlight LspReferenceWrite cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+augroup lsp_document_highlight
+  autocmd!
+  autocmd CursorHold,CursorHoldI * lua vim.lsp.buf.document_highlight()
+  autocmd CursorMoved,CursorMovedI * lua vim.lsp.buf.clear_references()
+augroup END
+]]
+
+-- 3. completion (hrsh7th/nvim-cmp)
+local cmp = require("cmp")
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  sources = {
+    { name = "nvim_lsp" },
+    { name = "buffer" },
+    { name = "path" },
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<C-n>"] = cmp.mapping.select_next_item(),
+    ['<C-l>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ["<CR>"] = cmp.mapping.confirm { select = true },
+  }),
+  experimental = {
+    ghost_text = true,
+  },
+})
+
+vim.cmd([[ colorscheme nightfox ]])
+
