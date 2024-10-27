68a27ba913985808f99da13d1439edb62dc3e0d0
ningen <ningen361@gmail.com>
2024-10-27 12:59:30 +0900

    added telescope

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -24,11 +24,6 @@ vim.g.maplocalleader = "\\"
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
-    -- add your plugins here
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+    { import = "plugins" }
+  }
 })
