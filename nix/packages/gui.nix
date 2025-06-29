20a4d9b176b5d0b5acc7e436784e014cafb28772
ningen <ningen361@gmail.com>
2025-06-29 23:28:21 +0900

    add Firefox MPRIS configuration to gui.nix

--- nix/packages/gui.nix
+++ nix/packages/gui.nix
@@ -1,6 +1,5 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
-    firefox
     discord
     hyprland
     xdg-desktop-portal-hyprland
@@ -12,4 +11,17 @@
     alacritty
     playerctl
   ];
+
+  # Firefox設定（MPRIS対応）
+  programs.firefox = {
+    enable = true;
+    profiles.default = {
+      settings = {
+        # MPRIS (Media Player Remote Interfacing Specification) 有効化
+        "media.hardwaremediakeys.enabled" = true;
+        "dom.media.mediasession.enabled" = true;
+        "media.mediasession.enabled" = true;
+      };
+    };
+  };
 }
