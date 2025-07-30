cb6da1eac3808577ac017406bafa2550209e96fd
ningen <ningen361@gmail.com>
2025-07-30 23:52:50 +0900

    install leaf package manager

---
+++ .config/emacs/early-init.el
@@ -0,0 +1,13 @@
+;; 画面最大化
+(push '(fullscreen . maximized) default-frame-alist)
+
+;; スクロール非表示
+(push '(vertical-scroll-bars) default-frame-alist)
+
+;; メニューバー非表示
+(push '(menu-bar-lines . 0) default-frame-alist)
+
+;; ツールバー非表示
+(push '(tool-bar-lines . 0) default-frame-alist)
+
+(provide 'early-init)
