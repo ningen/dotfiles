9a346cb200e9be57b22466906ab8ffd597204ad5
ningen <ningen361@gmail.com>
2025-07-30 21:12:40 +0900

    init.el

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,7 +1,21 @@
-(use-package evil
-	:ensure t
-	:config
-		(evil-mode 1))
+; ref: https://a.conao3.com/blog/2024/7c7c265/
+(eval-and-compile
+  (customize-set-variable
+   'package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                       ("melpa" . "https://melpa.org/packages/")))
+  (package-initialize)
+  (use-package leaf :ensure t)
+
+  (leaf leaf-keywords
+    :ensure t
+    :init
+    (leaf blackout :ensure t)
+    :config
+    (leaf-keywords-init)))
+
+(leaf leaf-convert
+  :doc "Convert many format to leaf format"
+  :ensure t)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -14,3 +28,112 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;cus-edit - customize の設定
+(leaf cus-edit
+  :doc "tools for customizing Emacs and Lisp packages"
+  :custom `((custom-file . ,(locate-user-emacs-file "custom.el"))))
+
+;cus-start - EmacsのC言語部分で定義された変数などの定義
+(leaf cus-start
+  :doc "define customization properties of builtins"
+  :preface
+  (defun c/redraw-frame nil
+    (interactive)
+    (redraw-frame))
+
+  :bind (("M-ESC ESC" . c/redraw-frame))
+  :custom '((user-full-name . "ningen")
+            (user-mail-address . "ningen361@gmail.com")
+            (user-login-name . "ningen")
+            (create-lockfiles . nil)
+            (tab-width . 4)
+            (debug-on-error . t)
+            (init-file-debug . t)
+            (frame-resize-pixelwise . t)
+            (enable-recursive-minibuffers . t)
+            (history-length . 1000)
+            (history-delete-duplicates . t)
+            (scroll-preserve-screen-position . t)
+            (scroll-conservatively . 100)
+            (mouse-wheel-scroll-amount . '(1 ((control) . 5)))
+            (ring-bell-function . 'ignore)
+            (text-quoting-style . 'straight)
+            (truncate-lines . t)
+            (use-dialog-box . nil)
+            (use-file-dialog . nil)
+            (menu-bar-mode . t)
+            (tool-bar-mode . nil)
+            (scroll-bar-mode . nil)
+            (indent-tabs-mode . nil))
+  :config
+  (defalias 'yes-or-no-p 'y-or-n-p)
+  (keyboard-translate ?\C-h ?\C-?))
+
+;autorevert - emacs 外でファイルが更新されたときに更新する
+(leaf autorevert
+  :doc "revert buffers when files on disk change"
+  :global-minor-mode global-auto-revert-mode)
+
+;delsel - 選択状態で入力したときに選択範囲を消去する
+(leaf delsel
+  :doc "delete selection if you insert"
+  :global-minor-mode delete-selection-mode)
+
+;paren - 括弧のハイライト
+(leaf paren
+  :doc "highlight matching paren"
+  :global-minor-mode show-paren-mode)
+
+;simple - 編集機能の設定
+(leaf simple
+  :doc "basic editing commands for Emacs"
+  :custom ((kill-read-only-ok . t)
+           (kill-whole-line . t)
+           (eval-expression-print-length . nil)
+           (eval-expression-print-level . nil)))
+
+;files - ファイル入出力の設定
+(leaf files
+  :doc "file input and output commands for Emacs"
+  :global-minor-mode auto-save-visited-mode
+  :custom `((auto-save-file-name-transforms . '((".*" ,(locate-user-emacs-file "backup/") t)))
+            (backup-directory-alist . '((".*" . ,(locate-user-emacs-file "backup"))
+                                        (,tramp-file-name-regexp . nil)))
+            (version-control . t)
+            (delete-old-versions . t)
+            (auto-save-visited-interval . 1)))
+
+;startup - 起動時の設定
+(leaf startup
+  :doc "process Emacs shell arguments"
+  :custom `((auto-save-list-file-prefix . ,(locate-user-emacs-file "backup/.saves-"))))
+
+;savehist - ミニバッファ履歴の保存
+(leaf savehist
+  :doc "Save minibuffer history"
+  :custom `((savehist-file . ,(locate-user-emacs-file "savehist")))
+  :global-minor-mode t)
+
+;flymake - コード診断機能
+(leaf flymake
+  :doc "A universal on-the-fly syntax checker"
+  :bind ((prog-mode-map
+          ("M-n" . flymake-goto-next-error)
+          ("M-p" . flymake-goto-prev-error))))
+
+;which-key キーバインドの補完表示
+(leaf which-key
+  :doc "Display available keybindings in popup"
+  :ensure t
+  :global-minor-mode t)
+
+;exec-path-from-shell - シェルから環境変数を引き継ぐ
+(leaf exec-path-from-shell
+  :doc "Get environment variables such as $PATH from the shell"
+  :ensure t
+  :defun (exec-path-from-shell-initialize)
+  :custom ((exec-path-from-shell-check-startup-files)
+           (exec-path-from-shell-variables . '("PATH" "GOPATH" "JAVA_HOME")))
+  :config
+  (exec-path-from-shell-initialize))
