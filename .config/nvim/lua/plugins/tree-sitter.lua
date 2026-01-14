bd3168be6d4c25c8e21ad51105562a5b7c5e945b
ningen <ningen361@gmail.com>
2026-01-14 22:59:22 +0900

    fixed

--- .config/nvim/lua/plugins/tree-sitter.lua
+++ .config/nvim/lua/plugins/tree-sitter.lua
@@ -1,5 +1,15 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  lazy = false,
-  build = ":TSUpdate",
+  "https://github.com/nvim-treesitter/nvim-treesitter",
+  branch = "main",
+  config = function()
+    require("nvim-treesitter").setup()
+
+    -- 自動ハイライトの有効化
+    vim.api.nvim_create_autocmd("FileType", {
+      group = vim.api.nvim_create_augroup("vim-treesitter-start", {}),
+      callback = function()
+        pcall(vim.treesitter.start)
+      end,
+    })
+  end,
 }
