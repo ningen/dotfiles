674c77c6baecca1a1104ba156ac379cf22fd524c
ningen <ningen361@gmail.com>
2026-05-05 00:32:40 +0900

    added

--- nix/hosts/common/home.nix
+++ nix/hosts/common/home.nix
@@ -71,6 +71,7 @@
             function ghq-fzf() {
               local repo
               repo=$(ghq list -p | fzf) && cd "$repo"
+              zle reset-prompt
             }
             zle -N ghq-fzf
             bindkey '^g' ghq-fzf
