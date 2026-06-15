f872082e95bf9fcf352ed0f5e402d0bd4665009c
ningen <ningen361@gmail.com>
2026-06-15 23:08:41 +0900

    delete init.el

--- .config/emacs/early-init.el
+++
@@ -1,6 +0,0 @@
-;;; early-init.el --- Early startup settings -*- lexical-binding: t; -*-
-
-;; Keep Emacs from eagerly loading package.el before init.el decides what to do.
-(setq package-enable-at-startup nil)
-
-;;; early-init.el ends here
