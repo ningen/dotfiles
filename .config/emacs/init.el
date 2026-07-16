c17b7b4b92a1aab6623a43a72cde2dccb35523aa
ningen <ningen361@gmail.com>
2026-07-16 23:22:03 +0900

    updated

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -2,4 +2,14 @@
 
 (global-display-line-numbers-mode 1)
 
+(use-package doom-themes
+  :custom
+  (doom-themes-enable-italic t)
+  (doom-themes-enable-bold t)
+  :custom-face
+  (doom-modeline-bar ((t (:background "#6272a4"))))
+  :config
+  (load-theme 'doom-dracula t)
+  (doom-themes-org-config))
+
 ;;; init.el ends here
