17d478bc8ca291aa7c0cef6a39bd5dee99eb1b99
ningen <ningen361@gmail.com>
2026-08-01 09:24:38 +0900

    delete unused plugin

--- .config/nvim/lua/plugins/memo.lua
+++
@@ -1,9 +0,0 @@
-return {
-  dir = "~/ghq/github.com/ningen/memo.nvim", -- GitHubにpushしたものをローカルで参照
-  config = function()
-    require("memo").setup()
-  end,
-  dependencies = {
-    "nvim-lua/plenary.nvim"
-  }
-}
