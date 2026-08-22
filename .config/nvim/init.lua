8421ebcba3277b48c7fad50f8e9d4ecfa97cec1f
ningen <ningen361@gmail.com>
2026-08-22 12:49:02 +0900

    updatd

---
+++ .config/nvim/init.lua
@@ -0,0 +1,12 @@
+require('vim._core.ui2').enable()	
+
+vim.o.number = true
+vim.o.relativenumber = true
+
+vim.keymap.set('i', 'jj', '<ESC>')
+vim.cmd('colorscheme catppuccin')
+
+
+require('config.lazy')
+
+
---
+++ .config/nvim/lua/plugins/filer.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "stevearc/oil.nvim",
+    --@module 'oil'
+    --@type oil.SetupOpts
+    opts = {},
+    dependencies = {
+      { "nvim-mini/mini.icons", opts = {} }
+    },
+    lazy = false
+  }
+}
