9c46c56f6004913c46f63e3559defd902d58085d
ningen <ningen361@gmail.com>
2026-08-23 21:31:54 +0900

    updated stylua

--- .config/nvim/lua/lsp/lua_ls.lua
+++ .config/nvim/lua/lsp/lua_ls.lua
@@ -1,10 +1,17 @@
 return {
   cmd = { 'lua-language-server' },
   filetypes = { 'lua' },
+  root_markers = { '.luarc.json', '.luarc.jsonc', '.stylua.toml', '.git' },
   settings = {
     Lua = {
+      runtime = { version = 'LuaJIT' },
       diagnostics = {
+        globals = { 'vim' },
         unusedLocalExclude = { '_*' }
+      },
+      workspace = {
+        checkThirdParty = false,
+        library = vim.api.nvim_get_runtime_file('', true)
       }
     }
   }
