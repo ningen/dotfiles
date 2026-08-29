4b86e0a3911edb20b32b02206920d6e3083deff3
ningen <ningen361@gmail.com>
2026-08-29 15:00:21 +0900

    feat(nvim): add blink completion

--- _config/nvim/lua/plugins/init.lua
+++ _config/nvim/lua/plugins/init.lua
@@ -3,6 +3,10 @@
 vim.pack.add({
   { src = "https://github.com/nvim-mini/mini.icons" },
   {
+    src = "https://github.com/Saghen/blink.cmp",
+    version = vim.version.range("1"),
+  },
+  {
     src = "https://github.com/stevearc/oil.nvim",
     name = "oil",
   },
@@ -16,6 +20,15 @@ vim.pack.add({
 })
 
 require("mini.icons").setup({})
+
+require("blink.cmp").setup({
+  keymap = { preset = "enter" },
+  appearance = { nerd_font_variant = "mono" },
+  completion = { documentation = { auto_show = true } },
+  sources = { default = { "lsp", "path", "snippets", "buffer" } },
+  fuzzy = { implementation = "prefer_rust_with_warning" },
+})
+
 local oil = require("oil")
 oil.setup({})
 
